fb6b0569634e5388ba4704217b629b9eeec4d63e
Fitriawan-Arya-N <nugrahaaryaa17@gmail.com>
2025-01-10 16:03:03 +0700

    update: terraform config

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -11,48 +11,10 @@ resource "google_container_cluster" "primary" {
     }
 }
 
-# Node pool untuk Frontend
-resource "google_container_node_pool" "frontend" {
-    name            = "${var.gke_cluster_name}-frontend"
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+    name            = "${var.gke_cluster_name}-preemptible-nodes"
     location        = var.gke_location
-    cluster         = google_container_cluster.primary.name
-    node_count      = var.node_count
 
-    node_config {
-        preemptible     = true
-        machine_type    = var.machine_type_node
-        disk_size_gb    = var.machine_disk_node
-        disk_type       = var.disk_type_node
-        service_account = var.service_account
-        oauth_scopes    = [
-            "https://www.googleapis.com/auth/cloud-platform",
-        ]
-    }
-}
-
-# Node pool untuk Backend
-resource "google_container_node_pool" "backend" {
-    name            = "${var.gke_cluster_name}-backend"
-    location        = var.gke_location
-    cluster         = google_container_cluster.primary.name
-    node_count      = var.node_count
-
-    node_config {
-        preemptible     = true
-        machine_type    = var.machine_type_node
-        disk_size_gb    = var.machine_disk_node
-        disk_type       = var.disk_type_node
-        service_account = var.service_account
-        oauth_scopes    = [
-            "https://www.googleapis.com/auth/cloud-platform",
-        ]
-    }
-}
-
-# Node pool untuk Database
-resource "google_container_node_pool" "database" {
-    name            = "${var.gke_cluster_name}-database"
-    location        = var.gke_location
     cluster         = google_container_cluster.primary.name
     node_count      = var.node_count
 
